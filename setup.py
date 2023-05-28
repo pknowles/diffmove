409a63f3dcc7e6832424dccd17df57665c71ad22
pknowles <pknowles@users.noreply.github.com>
2023-05-27 17:57:20 -0700

    bump to version 0.2

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@
 from distutils.core import setup
 
 setup(name='diffmove',
-	version='0.1',
+	version='0.2',
 	description='Primitive extension to difflib supporting move detection',
 	author='Pyarelal Knowles',
 	author_email='pknowles@users.noreply.github.com',
 	url='https://github.com/pknowles/diffmove',
 	packages=['diffmove'],
 	)
-
